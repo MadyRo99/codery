20ae60b438af7b6757b208e5222ae09865c8c6bb
Madalin <madalindanescu99@gmail.com>
2020-09-28 17:40:42 +0300

    added first page responsive header + added cookies consent banner

---
+++ resources/views/partials/cookies.blade.php
@@ -0,0 +1,9 @@
+<div class="alert alert-dismissible text-center cookiealert" role="alert">
+    <div class="cookiealert-container">
+        Prin utilizarea acestui site, îți dai acordul cu privire la faptul că ai citit și ai înțeles <a href="#">Politica privind cookie-urile</a>,
+        <a href="#">Politica de confidențialitate</a> și <a href="#">Termenii și condițiile de utilizare</a>.
+        <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">
+            Accept
+        </button>
+    </div>
+</div>
